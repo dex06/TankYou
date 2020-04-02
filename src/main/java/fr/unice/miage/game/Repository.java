c2d68972fc39e4ebb231684e3be0b9dd409ef980
Derek <derek.samson@etu.unice.fr>
2020-04-02 19:17:14 +0200

    added .gitignore

--- src/main/java/fr/unice/miage/game/Repository.java
+++ src/main/java/fr/unice/miage/game/Repository.java
@@ -1,12 +1,26 @@
 package fr.unice.miage.game;
 
 import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLClassLoader;
 
 public class Repository {
-    public Repository(File base){
+    ClassLoader loader = new URLClassLoader(new URL[]{new File("fr/unice/miage/game/classes").toURL()});
 
+    public Repository() throws MalformedURLException {}
+    public Repository(File base) throws MalformedURLException {
+        this.loader = new URLClassLoader(new URL[] {base.toURL()});
     }
    /* public List<Class<?>> loadMovement(String path){
+
+    } */
+
+   /* public List<Class<?>> loadWeapon(String path){
+
+    } */
+
+   /* public List<Class<?>> loadGraphic(String path){
 
     } */
 }
