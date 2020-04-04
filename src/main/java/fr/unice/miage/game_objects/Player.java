393b88d52822c206c7dc8c934db59dedf80d5a8e
Derek <derek.samson@etu.unice.fr>
2020-04-04 17:29:27 +0200

    modified class Player

--- src/main/java/fr/unice/miage/game_objects/Player.java
+++ src/main/java/fr/unice/miage/game_objects/Player.java
@@ -4,8 +4,12 @@ import fr.unice.miage.geom.Vector2;
 
 public class Player {
     Vector2 position;
+    Vector2 velocity;
+    Vector2 acceleration;
 
     public Player(){
         this.position = new Vector2();
+        this.velocity = new Vector2();
+        this.acceleration = new Vector2();
     }
 }
