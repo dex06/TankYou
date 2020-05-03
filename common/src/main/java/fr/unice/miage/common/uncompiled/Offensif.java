0f63f1a1232c558aa9a239055d9b8f45ce9121a5
Derek <derek.samson@etu.unice.fr>
2020-05-03 05:14:56 +0200

    Added Defensif and Offensif classes for PlugInMovement

---
+++ common/src/main/java/fr/unice/miage/common/uncompiled/Offensif.java
@@ -0,0 +1,62 @@
+package fr.unice.miage.common.uncompiled;
+
+import fr.unice.miage.common.game_objects.Player;
+import fr.unice.miage.common.geom.Vector2;
+import fr.unice.miage.common.plugins.PlugInMovement;
+import fr.unice.miage.common.utils.Finder;
+import fr.unice.miage.common.utils.Randomizer;
+
+public class Offensif implements PlugInMovement {
+    public void init(Player player){
+        player.setPosition(new Vector2(100,100));
+        player.setVelocity(new Vector2(0.1,0.1));
+        player.setAcceleration(new Vector2());
+        player.setRotation(0);
+    }
+
+    public void move(Player player){
+        playerMove(player);
+    }
+
+    public void playerMove(Player player){
+        int[] cpmts = {1,1,1,2,2,2,2,2,2,3,3,3};
+        int randCpmt = cpmts[(int) Math.ceil(Math.random() * cpmts.length)];
+        switch (randCpmt){
+            case 1 :
+                randomMove(player);
+                break;
+            case 2 :
+                seekMove(player);
+                break;
+            case 3:
+                fleeMove(player);
+                break;
+        }
+    }
+
+    public void randomMove(Player player){
+        //Random movements with varying acceleration between specified limits
+        player.addVelocity(Randomizer.getRandomVector(-0.2,0.2));
+        // Limiting the velocity
+        player.getVelocity().limit(2);
+        player.addPosition(player.getVelocity());
+    }
+
+    public void seekMove(Player player){
+        Vector2 targetPosition = Finder.findClosestPlayer(player).getPosition();
+        Vector2 desired_velocity = targetPosition.sub2(player.getPosition()).norm2().mult2(player.getMaxVelocity());
+        Vector2 steering = desired_velocity.sub2(player.getVelocity());
+        steering = steering.limit2(player.getMaxForce()).mult2(1/player.getMass());
+        Vector2 velocity = player.getVelocity().add2(steering).limit2(player.getMaxSpeed());
+        player.setPosition(player.getPosition().add2(velocity));
+    }
+
+    public void fleeMove(Player player){
+        Vector2 targetPosition = Finder.findClosestPlayer(player).getPosition();
+        Vector2 desired_velocity = targetPosition.sub2(player.getPosition()).norm2().mult2(player.getMaxVelocity());
+        Vector2 steering = desired_velocity.sub2(player.getVelocity()).reverse2();
+        steering = steering.limit2(player.getMaxForce()).mult2(1/player.getMass());
+        Vector2 velocity = player.getVelocity().add2(steering).limit2(player.getMaxSpeed());
+        player.setPosition(player.getPosition().add2(velocity));
+    }
+}
