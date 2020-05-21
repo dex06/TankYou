5f93d20e1312c40ff534ac20d3a04e61f8ef2384
Derek <derek.samson@etu.unice.fr>
2020-05-21 18:46:06 +0200

    Progressing on GameFiles

--- common/src/main/java/fr/unice/miage/common/Repository.java
+++ common/src/main/java/fr/unice/miage/common/Repository.java
@@ -15,10 +15,12 @@ import java.util.List;
 import java.util.jar.JarEntry;
 import java.util.jar.JarFile;
 import java.util.jar.JarOutputStream;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
 
 public class Repository {
 
-    private final List<File> jarFiles = new ArrayList<>();
+    private final List<File> archiveFiles = new ArrayList<>();
     private final ObservableList<String> movePluginsNames = FXCollections.observableArrayList();
     private final ObservableList<String> weaponPluginsNames = FXCollections.observableArrayList();
     private final ObservableList<String> graphicPluginsNames = FXCollections.observableArrayList();
@@ -48,7 +50,7 @@ public class Repository {
 
     public Repository(String base) throws Exception { this.loadLibraries(base); }
 
-    public List<File> getJarFiles(){ return jarFiles; }
+    public List<File> getJarFiles(){ return archiveFiles; }
 
     public ObservableList<String> getMovePluginsNames() {
         return movePluginsNames;
@@ -77,86 +79,109 @@ public class Repository {
 
     public void loadLibraries(String path) throws Exception {
         File libDir = new File(path);
-        if(isJarFile(libDir)) jarFiles.add(libDir);
+        if(isJarFile(libDir)) archiveFiles.add(libDir);
         else if (!libDir.exists() || !libDir.isDirectory()) {
             throw new RuntimeException("Invalid library directory");
         }
         if(libDir.isDirectory()) {
             for (File file : libDir.listFiles()) {
-                if (!isJarFile(file)) {
+                if (!isJarFile(file) & !isZipFile(file)) {
                     continue;
                 }
                 System.out.println("Found library format : " + file.getName());
-                jarFiles.add(file);
+                archiveFiles.add(file);
             }
         }
-        loadClassesFromJarFiles();
+        loadClassesFromArchiveFiles();
     }
 
 
-    public void loadClassesFromJarFiles() throws IOException, ClassNotFoundException, NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException {
-        MyClassLoader cl = new MyClassLoader(jarFiles);
-        for(File jar : jarFiles) {
-            JarFile jarFile = new JarFile(jar);
-            Enumeration<JarEntry> e = jarFile.entries();
-            while (e.hasMoreElements()) {
-                JarEntry je = e.nextElement();
-                if (je.isDirectory() || !je.getName().endsWith(".class") || je.getName().contains("module-info.class")) {
-                    continue;
-                }
-                System.out.println("Loading jar class : " + je.getRealName());
-                //String pack = packageName + "." + appFolderName + "." + je.getName().replace(".class", "");
-                String pack = je.getName().replace("/", ".").replace(".class", "");
-                Class loadedClass = cl.loadClass(pack);
+    public void loadClassesFromArchiveFiles() throws IOException, ClassNotFoundException, NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException {
+        MyClassLoader cl = new MyClassLoader(archiveFiles);
+        for(File archive : archiveFiles) {
 
-                Object instance = loadedClass.getDeclaredConstructor().newInstance();
-                String interfaceName = instance.getClass().getInterfaces()[0].getSimpleName();
-
-                switch (interfaceName) {
-                    case "PlugInMovement":
-                        movePluginsNames.add(instance.getClass().getSimpleName());
-                        movePlugins.add(loadedClass);
-                        break;
-                    case "PlugInWeapon":
-                        weaponPluginsNames.add(instance.getClass().getSimpleName());
-                        weaponPlugins.add(loadedClass);
-                        break;
-                    case "PlugInGraphic":
-                        graphicPluginsNames.add(instance.getClass().getSimpleName());
-                        graphicPlugins.add(loadedClass);
-                        break;
-                    case "PlugInCollision":
-                        collisionPluginsNames.add(instance.getClass().getSimpleName());
-                        collisionPlugins.add(loadedClass);
-                        break;
-                    case "PlugInObstacle":
-                        obstaclePluginsNames.add(instance.getClass().getSimpleName());
-                        obstaclePlugins.add(loadedClass);
-                        break;
-                    case "PlugInBackground":
-                        backgroundPluginsNames.add(instance.getClass().getSimpleName());
-                        backgroundPlugins.add(loadedClass);
-                        break;
-                    case "PlugInGUI1":
-                        gui1PluginsNames.add(instance.getClass().getSimpleName());
-                        gui1Plugins.add(loadedClass);
-                        break;
-                    case "PlugInGUI2":
-                        gui2PluginsNames.add(instance.getClass().getSimpleName());
-                        gui2Plugins.add(loadedClass);
-                        break;
-                    case "PlugInRealPlayer":
-                        realPlayerPluginsNames.add(instance.getClass().getSimpleName());
-                        realPlayerPlugins.add(loadedClass);
-                        break;
-
+            if(isZipFile(archive)){
+                ZipFile zipFile = new ZipFile(archive);
+                Enumeration<ZipEntry> zipEntries = (Enumeration<ZipEntry>) zipFile.entries();
+                while (zipEntries.hasMoreElements()) {
+                    ZipEntry je = zipEntries.nextElement();
+                    if (je.isDirectory() || !je.getName().endsWith(".class") || je.getName().contains("module-info.class")) {
+                        continue;
+                    }
+                    System.out.println("Loading jar class : " + je.getName());
+                    String pack = je.getName().replace("/", ".").replace(".class", "");
+                    loadClassFromPath(cl, pack);
                 }
             }
+            else {
+                JarFile jarFile = new JarFile(archive);
+                Enumeration<JarEntry> jarEntries = jarFile.entries();
+                while (jarEntries.hasMoreElements()) {
+                    JarEntry je = jarEntries.nextElement();
+                    if (je.isDirectory() || !je.getName().endsWith(".class") || je.getName().contains("module-info.class")) {
+                        continue;
+                    }
+                    System.out.println("Loading jar class : " + je.getRealName());
+                    String pack = je.getName().replace("/", ".").replace(".class", "");
+                    loadClassFromPath(cl, pack);
+                }
+            }
+
         }
     }
 
+    private void loadClassFromPath(MyClassLoader cl, String path) throws ClassNotFoundException, NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException {
+        Class loadedClass = cl.loadClass(path);
+
+        Object instance = loadedClass.getDeclaredConstructor().newInstance();
+        String interfaceName = instance.getClass().getInterfaces()[0].getSimpleName();
+
+        switch (interfaceName) {
+            case "PlugInMovement":
+                movePluginsNames.add(instance.getClass().getSimpleName());
+                movePlugins.add(loadedClass);
+                break;
+            case "PlugInWeapon":
+                weaponPluginsNames.add(instance.getClass().getSimpleName());
+                weaponPlugins.add(loadedClass);
+                break;
+            case "PlugInGraphic":
+                graphicPluginsNames.add(instance.getClass().getSimpleName());
+                graphicPlugins.add(loadedClass);
+                break;
+            case "PlugInCollision":
+                collisionPluginsNames.add(instance.getClass().getSimpleName());
+                collisionPlugins.add(loadedClass);
+                break;
+            case "PlugInObstacle":
+                obstaclePluginsNames.add(instance.getClass().getSimpleName());
+                obstaclePlugins.add(loadedClass);
+                break;
+            case "PlugInBackground":
+                backgroundPluginsNames.add(instance.getClass().getSimpleName());
+                backgroundPlugins.add(loadedClass);
+                break;
+            case "PlugInGUI1":
+                gui1PluginsNames.add(instance.getClass().getSimpleName());
+                gui1Plugins.add(loadedClass);
+                break;
+            case "PlugInGUI2":
+                gui2PluginsNames.add(instance.getClass().getSimpleName());
+                gui2Plugins.add(loadedClass);
+                break;
+            case "PlugInRealPlayer":
+                realPlayerPluginsNames.add(instance.getClass().getSimpleName());
+                realPlayerPlugins.add(loadedClass);
+                break;
+
+        }
+    }
     private boolean isJarFile(File file) {
         return file.getName().endsWith(".jar");
+    }
+
+    private boolean isZipFile(File file){
+        return file.getName().endsWith(".zip");
     }
 
 
@@ -241,7 +266,7 @@ public class Repository {
                 fileName = destinationDir + File.separator + entry.getName();
                 f = new File(fileName);
                 //Adding to list of jar files
-                //jarFiles.add(f);
+                //archiveFiles.add(f);
                 if (!fileName.endsWith("/")) {
                     InputStream is = jar.getInputStream(entry);
                     FileOutputStream fos = new FileOutputStream(f);
--- engine/src/main/java/fr/unice/miage/engine/gui/GameFiles.java
+++ engine/src/main/java/fr/unice/miage/engine/gui/GameFiles.java
@@ -14,6 +14,9 @@ import javafx.scene.control.TextField;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.stage.DirectoryChooser;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
@@ -26,6 +29,7 @@ import java.util.jar.JarFile;
 public class GameFiles extends Application {
 
     private VBox selectorVBox;
+    private HBox pluginsHBox;
     private final List<Repository> repoList = new ArrayList<>();
     private Repository repository;
     private final List<String> listOfGUI1Options = new ArrayList<>();
@@ -47,19 +51,28 @@ public class GameFiles extends Application {
         primaryStage.setTitle("Plugin selection");
 
         FileChooser fileChooser = new FileChooser();
+        DirectoryChooser directoryChooser = new DirectoryChooser();
+
         String currentPath = Paths.get(".").toAbsolutePath().normalize().toString();
+        File currentFilePath = new File(currentPath);
         String userDirectoryString = System.getProperty("user.home");
-        File userDirectory = new File(currentPath);
+        File userDirectory = new File(userDirectoryString);
         if(!userDirectory.canRead()) {
             userDirectory = new File("c:/");
         }
-        fileChooser.setInitialDirectory(userDirectory);
 
-        fileChooser.getExtensionFilters().addAll(
-                new FileChooser.ExtensionFilter("Jar Files", "*.jar")
-        );
 
-        Label txtFldLabel = new Label("Enter path to jar");
+        fileChooser.setInitialDirectory(currentFilePath);
+        FileChooser.ExtensionFilter jarFilter = new FileChooser.ExtensionFilter("Jar or Zip Files", "*.jar", "*.zip");
+
+        fileChooser.getExtensionFilters().addAll(jarFilter);
+        fileChooser.setSelectedExtensionFilter(jarFilter);
+
+        directoryChooser.setTitle("Select directory");
+        directoryChooser.setInitialDirectory(userDirectory);
+
+
+        Label txtFldLabel = new Label("Enter path to jar, zip or dir");
         TextField textField = new TextField();
         textField.setMinWidth(400);
         Button okBtn = new Button("OK");
@@ -76,11 +89,12 @@ public class GameFiles extends Application {
                 System.err.println(err);
             }
         });
-        Button chooserBtn = new Button("Import Jar File");
+        Button chooserBtn = new Button("Select Jar or Zip File");
         chooserBtn.setOnAction(e -> {
             File selectedFile = fileChooser.showOpenDialog(primaryStage);
             if(selectedFile != null) {
-                if (selectedFile.getName().endsWith(".jar")) {
+                textField.setText(selectedFile.getAbsolutePath());
+                if (selectedFile.getName().endsWith(".jar") || selectedFile.getName().endsWith(".zip")) {
                     try {
                         repository.loadLibraries(selectedFile.getCanonicalPath());
                         displayRepos();
@@ -88,8 +102,27 @@ public class GameFiles extends Application {
                         exception.printStackTrace();
                     }
                 }
+            } else {
+                textField.setText(null);
             }
         });
+
+        Button dirChooserBtn = new Button("Select directory");
+        dirChooserBtn.setOnAction(e -> {
+            File dir = directoryChooser.showDialog(primaryStage);
+            if (dir != null) {
+                textField.setText(dir.getAbsolutePath());
+                try {
+                    repository.loadLibraries(dir.getCanonicalPath());
+                    displayRepos();
+                } catch (Exception exception) {
+                    exception.printStackTrace();
+                }
+            } else {
+                textField.setText(null);
+            }
+        });
+
         Button copyBtn = new Button("Copy Jar File");
         copyBtn.setOnAction(e -> {
             File selectedFile = fileChooser.showOpenDialog(primaryStage);
@@ -110,14 +143,15 @@ public class GameFiles extends Application {
         HBox fieldHBox = new HBox(textField);
         HBox okHBox = new HBox(okBtn);
         HBox chooserHBox = new HBox(chooserBtn);
+        HBox dirChooserHBox = new HBox(dirChooserBtn);
         HBox copyHBox = new HBox(copyBtn);
-        selectHBox.getChildren().addAll(txtFldLabel, fieldHBox, okHBox, chooserHBox, copyHBox);
+        selectHBox.getChildren().addAll(txtFldLabel, fieldHBox, okHBox, chooserHBox, dirChooserHBox, copyHBox);
         selectHBox.setPadding(new Insets(10,10,10,10));
         selectHBox.setSpacing(10);
         selectHBox.setAlignment(Pos.TOP_CENTER);
 
         selectorVBox = new VBox();
-
+        pluginsHBox = new HBox();
 
         displayRepos();
         VBox mainVBox = new VBox();
@@ -133,7 +167,11 @@ public class GameFiles extends Application {
             gameEngine.initMenu();
             gameEngine.startMenu();
         });
-        mainVBox.getChildren().addAll(selectHBox, selectorVBox, gotoMenuBtn);
+
+
+
+
+        mainVBox.getChildren().addAll(selectHBox, selectorVBox, pluginsHBox, gotoMenuBtn);
         Scene scene = new Scene(root, 960, 600);
 
 
@@ -144,12 +182,122 @@ public class GameFiles extends Application {
 
     public void displayRepos() {
         List<File> jarFiles = repository.getJarFiles();
+        Label archiveLabel = new Label("Archives");
+        archiveLabel.setFont(Font.font("Verdana", FontWeight.BOLD, 12));
         for (File jar : jarFiles) {
+            if(selectorVBox.getChildren().isEmpty()) selectorVBox.getChildren().add(archiveLabel);
+
             CheckBox checkBox = new CheckBox(jar.getName());
+            checkBox.setSelected(true);
             selectorVBox.getChildren().add(checkBox);
         }
-
+        if(!pluginsHBox.getChildren().isEmpty()) pluginsHBox = new HBox();
+        if(!jarFiles.isEmpty()) displayPlugins();
+        selectorVBox.setSpacing(5);
         //selectorVBox.setStyle("-fx-background-color: #336699;");
+    }
+
+    public void displayPlugins(){
+        int spacing = 5;
+        String font = "Verdana";
+        int fontSize = 12;
+
+        VBox moveVBox = new VBox();
+        moveVBox.setSpacing(spacing);
+        VBox graphicVBox = new VBox();
+        graphicVBox.setSpacing(spacing);
+        VBox weaponVBox = new VBox();
+        weaponVBox.setSpacing(spacing);
+        VBox collisionVBox = new VBox();
+        collisionVBox.setSpacing(spacing);
+        VBox obstacleVBox = new VBox();
+        obstacleVBox.setSpacing(spacing);
+        VBox backgroundVBox = new VBox();
+        backgroundVBox.setSpacing(spacing);
+        VBox gui1VBox = new VBox();
+        gui1VBox.setSpacing(spacing);
+        VBox gui2VBox = new VBox();
+        gui2VBox.setSpacing(spacing);
+        VBox realPlayerVBox = new VBox();
+        realPlayerVBox.setSpacing(spacing);
+
+        for(String name : repository.getMovePluginsNames()) {
+            Label moveLabel = new Label("Moves");
+            moveLabel.setFont(Font.font(font, FontWeight.BOLD, fontSize));
+            if(moveVBox.getChildren().isEmpty()) moveVBox.getChildren().add(moveLabel);
+            CheckBox checkBox = new CheckBox(name);
+            checkBox.setSelected(true);
+            moveVBox.getChildren().add(checkBox);
+        }
+        for(String name : repository.getGraphicPluginsNames()) {
+            Label graphicLabel = new Label("Graphics");
+            graphicLabel.setFont(Font.font(font, FontWeight.BOLD, fontSize));
+            if(graphicVBox.getChildren().isEmpty()) graphicVBox.getChildren().add(graphicLabel);
+            CheckBox checkBox = new CheckBox(name);
+            checkBox.setSelected(true);
+            graphicVBox.getChildren().add(checkBox);
+        }
+        for(String name : repository.getWeaponPluginsNames()) {
+            Label weaponLabel = new Label("Weapons");
+            weaponLabel.setFont(Font.font(font, FontWeight.BOLD, fontSize));
+            if(weaponVBox.getChildren().isEmpty()) weaponVBox.getChildren().add(weaponLabel);
+            CheckBox checkBox = new CheckBox(name);
+            checkBox.setSelected(true);
+            weaponVBox.getChildren().add(checkBox);
+        }
+        for(String name : repository.getCollisionPluginsNames()) {
+            Label collisionLabel = new Label("Collisions");
+            collisionLabel.setFont(Font.font(font, FontWeight.BOLD, fontSize));
+            if(collisionVBox.getChildren().isEmpty()) collisionVBox.getChildren().add(collisionLabel);
+            CheckBox checkBox = new CheckBox(name);
+            checkBox.setSelected(true);
+            collisionVBox.getChildren().add(checkBox);
+        }
+        for(String name : repository.getObstaclePluginsNames()) {
+            Label obstacleLabel = new Label("Obstacles");
+            obstacleLabel.setFont(Font.font(font, FontWeight.BOLD, fontSize));
+            if(obstacleVBox.getChildren().isEmpty()) obstacleVBox.getChildren().add(obstacleLabel);
+            CheckBox checkBox = new CheckBox(name);
+            checkBox.setSelected(true);
+            obstacleVBox.getChildren().add(checkBox);
+        }
+        for(String name : repository.getBackgroundPluginsNames()) {
+            Label backgroundLabel = new Label("Backgrounds");
+            backgroundLabel.setFont(Font.font(font, FontWeight.BOLD, fontSize));
+            if(backgroundVBox.getChildren().isEmpty()) backgroundVBox.getChildren().add(backgroundLabel);
+            CheckBox checkBox = new CheckBox(name);
+            checkBox.setSelected(true);
+            backgroundVBox.getChildren().add(checkBox);
+        }
+        for(String name : repository.getGui1PluginsNames()) {
+            Label gui1Label = new Label("GUI1s");
+            gui1Label.setFont(Font.font(font, FontWeight.BOLD, fontSize));
+            if(gui1VBox.getChildren().isEmpty()) gui1VBox.getChildren().add(gui1Label);
+            CheckBox checkBox = new CheckBox(name);
+            checkBox.setSelected(true);
+            gui1VBox.getChildren().add(checkBox);
+        }
+        for(String name : repository.getGui2PluginsNames()) {
+            Label gui2Label = new Label("GUI2s");
+            gui2Label.setFont(Font.font(font, FontWeight.BOLD, fontSize));
+            if(gui2VBox.getChildren().isEmpty()) gui2VBox.getChildren().add(gui2Label);
+            CheckBox checkBox = new CheckBox(name);
+            checkBox.setSelected(true);
+            gui2VBox.getChildren().add(checkBox);
+        }
+        for(String name : repository.getRealPlayerPluginsNames()) {
+            Label realPlayerLabel = new Label("Real Player");
+            realPlayerLabel.setFont(Font.font(font, FontWeight.BOLD, fontSize));
+            if(realPlayerVBox.getChildren().isEmpty()) realPlayerVBox.getChildren().add(realPlayerLabel);
+            CheckBox checkBox = new CheckBox(name);
+            checkBox.setSelected(true);
+            realPlayerVBox.getChildren().add(checkBox);
+        }
+
+        pluginsHBox.getChildren().addAll(moveVBox, graphicVBox, weaponVBox, backgroundVBox, obstacleVBox, collisionVBox, gui1VBox, gui2VBox, realPlayerVBox);
+        //pluginsHBox.setSpacing(10);
+        //pluginsHBox.setPadding(new Insets(10,10,10,10));
+        //pluginsHBox.setAlignment(Pos.CENTER);
     }
 }
 
